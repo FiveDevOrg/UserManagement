b8c527ee1582dc35e8115a0dd65b96426c423540
IonutDoroftei <ionut.doroftei@assist.ro>
2022-10-09 18:01:10 +0300

    Add discovery server client setup

--- src/main/java/com/auxby/usermanager/UserManagerApplication.java
+++ src/main/java/com/auxby/usermanager/UserManagerApplication.java
@@ -2,8 +2,10 @@ package com.auxby.usermanager;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class UserManagerApplication {
 
 	public static void main(String[] args) {
