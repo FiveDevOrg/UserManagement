4bccaad5a488d41bbe1f3439eaed5b6825048c75
IonutDoroftei <ionut.doroftei@assist.ro>
2022-10-24 15:25:54 +0300

    Set address as optional and set email an phone as mandatory fields

--- src/main/java/com/auxby/usermanager/api/v1/user/model/UserDetailsInfo.java
+++ src/main/java/com/auxby/usermanager/api/v1/user/model/UserDetailsInfo.java
@@ -12,8 +12,10 @@ import static com.auxby.usermanager.utils.constant.AppConstant.VALID_PHONE_REGEX
 public record UserDetailsInfo(@NotBlank(message = "Last name is mandatory.") String lastName,
                               @NotBlank(message = "First name is mandatory.") String firstName,
                               @NotBlank(message = "Username is mantadory.") String userName,
+                              @NotBlank(message = "Email address is mandatory.")
                               @Pattern(regexp = VALID_EMAIL_REGEX, message = "Invalid email.") String email,
                               AddressInfo address,
+                              @NotBlank(message = "Phone number is mandatory.")
                               @Pattern(regexp = VALID_PHONE_REGEX, message = "Invalid phone number.")
                               String phone) {
 }
