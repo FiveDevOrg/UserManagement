f8bebf66f35e15db1c883089c763d7477bb1b5f0
IonutDoroftei <ionut.doroftei@assist.ro>
2022-11-15 21:36:35 +0200

    Update

--- src/main/java/com/auxby/usermanager/UserManagerApplication.java
+++ src/main/java/com/auxby/usermanager/UserManagerApplication.java
@@ -1,10 +1,8 @@
 package com.auxby.usermanager;
 
-import org.keycloak.adapters.springboot.KeycloakSpringBootConfigResolver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableEurekaClient
@@ -14,8 +12,4 @@ public class UserManagerApplication {
         SpringApplication.run(UserManagerApplication.class, args);
     }
 
-    @Bean
-    public KeycloakSpringBootConfigResolver keycloakConfigResolver() {
-        return new KeycloakSpringBootConfigResolver();
-    }
 }
