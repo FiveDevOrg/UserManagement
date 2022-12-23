74a4ca7c5f22f75a53c1539861bad8d0794b71b3
IonutDoroftei <ionut.doroftei@assist.ro>
2022-12-23 21:35:52 +0200

    Add stripe and configure endpoint

---
+++ src/main/java/com/auxby/usermanager/payment/PaymentController.java
@@ -0,0 +1,28 @@
+package com.auxby.usermanager.payment;
+
+import com.auxby.usermanager.payment.model.PaymentRequest;
+import com.auxby.usermanager.utils.constant.AppConstant;
+import com.stripe.exception.StripeException;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+@RequestMapping(value = AppConstant.BASE_V1_URL)
+public class PaymentController {
+
+    private final StripePaymentService stripePaymentService;
+
+    @PostMapping("/payment/charge")
+    public String createPaymentIntent(@Valid @RequestBody PaymentRequest paymentRequest) throws StripeException {
+        log.info("POST - trigger a payment.");
+        return stripePaymentService.charge(paymentRequest);
+    }
+}
---
+++ src/main/java/com/auxby/usermanager/utils/enums/Currency.java
@@ -0,0 +1,5 @@
+package com.auxby.usermanager.utils.enums;
+
+public enum Currency {
+    EUR, RON;
+}
