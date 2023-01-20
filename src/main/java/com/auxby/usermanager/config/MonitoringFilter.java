088432f3d5dcb93981ffdffba1f382d4a99221e8
IonutDoroftei <ionut.doroftei@assist.ro>
2023-01-20 16:23:19 +0200

    Add user activity monitoring filter

---
+++ src/main/java/com/auxby/usermanager/config/MonitoringFilter.java
@@ -0,0 +1,28 @@
+package com.auxby.usermanager.config;
+
+import com.amazonaws.util.StringUtils;
+import com.auxby.usermanager.api.v1.user.UserService;
+import com.auxby.usermanager.utils.SecurityContextUtil;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.*;
+import java.io.IOException;
+
+@Component
+@AllArgsConstructor
+public class MonitoringFilter implements Filter {
+
+    private final UserService userService;
+
+    @Override
+    public void doFilter(ServletRequest request,
+                         ServletResponse response,
+                         FilterChain chain) throws ServletException, IOException {
+        String uuid = SecurityContextUtil.getUserId();
+        if (StringUtils.hasValue(uuid)) {
+            userService.updateUserLastSeen(uuid);
+        }
+        chain.doFilter(request, response);
+    }
+}
