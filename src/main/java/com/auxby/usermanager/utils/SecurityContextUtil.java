4397e7de9c15e259075161206f0053acfcdd4abf
IonutDoroftei <ionut.doroftei@assist.ro>
2023-01-20 16:24:14 +0200

    Add user activity monitoring filter

--- src/main/java/com/auxby/usermanager/utils/SecurityContextUtil.java
+++ src/main/java/com/auxby/usermanager/utils/SecurityContextUtil.java
@@ -2,12 +2,17 @@ package com.auxby.usermanager.utils;
 
 import org.springframework.security.core.context.SecurityContextHolder;
 
+import java.util.Objects;
+
 public class SecurityContextUtil {
 
     private SecurityContextUtil(){}
 
     public static String getUserId() {
         var authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (Objects.isNull(authentication)) {
+            return null;
+        }
         return authentication.getName();
     }
 }
