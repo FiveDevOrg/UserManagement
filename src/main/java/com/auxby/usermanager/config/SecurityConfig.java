2526b41a65aaeb58b7666468628b379ddba9ce37
Ionut Doroftei <ionut.doroftei@assist.ro>
2023-04-06 13:50:32 +0300

    Update cors

--- src/main/java/com/auxby/usermanager/config/SecurityConfig.java
+++ src/main/java/com/auxby/usermanager/config/SecurityConfig.java
@@ -72,7 +72,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000", "https://auxby-web-offer-manager.herokuapp.com"));
+        configuration.setAllowedOrigins(Arrays.asList("*"));
         configuration.setAllowedMethods(Arrays.asList("*"));
         configuration.setAllowedHeaders(Arrays.asList("Content-Type", "Authorization"));
 
